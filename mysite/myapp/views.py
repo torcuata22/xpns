aa5960d5ba9413d25fc7d128e0c3f747715aa19b
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-29 14:42:06 -0600

    post method done

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Expense
+from .forms import ExpenseForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'myapp/index.html')
+    if request.method == "POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid:
+            expense.save()
+            
+    expense_form = ExpenseForm()
+    return render(request, 'myapp/index.html', {'expense_form':expense_form})
