bc44d43adde8471fe13f48aab6e7667cb7edab92
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-29 15:56:03 -0600

    finished edit functionality

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Expense
 from .forms import ExpenseForm
 
@@ -17,4 +17,13 @@ def index(request):
 def edit(request, id):
     expense = Expense.objects.get(id=id)
     expense_form = ExpenseForm(instance=expense) #instance is an attribute of the model form
+
+    if request.method == "POST":
+        expense = Expense.objects.get(id=id) #yes, this is the same expense variable as above, it could be refactored
+        form = ExpenseForm(request.POST, instance=expense) #because we want the new data that got passed to the form
+        
+        if form.is_valid:
+            form.save()
+            return redirect('index')
+    
     return render(request, 'myapp/edit.html', {'expense_form': expense_form})
