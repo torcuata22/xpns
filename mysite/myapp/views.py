52865e7622605d24084f063924fbf627ee6fd57c
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-30 13:54:29 -0600

    views for sums

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,21 +1,51 @@
-from django.shortcuts import render, redirect
-from .models import Expense
+from django.shortcuts import render,redirect
 from .forms import ExpenseForm
+from .models import Expense
+import datetime
 from django.db.models import Sum
-
 # Create your views here.
 def index(request):
-    if request.method == "POST":
+    if request.method =="POST":
         expense = ExpenseForm(request.POST)
-        if expense.is_valid:
+        if expense.is_valid():
             expense.save()
-
+            
     expenses = Expense.objects.all()
-    total_expenses = expenses.aggregate(Sum('amount')) #remember: amount is the name of the field in the model
-    print(total_expenses)
-
+    total_expenses = expenses.aggregate(Sum('amount'))
+    
+    #Logic to calculate 365 days expenses (1 year)
+    last_year = datetime.date.today() - datetime.timedelta(days=365) #calculates when last year ends
+    data = Expense.objects.filter(date__gt=last_year) #filters info so only this year is added
+    yearly_sum = data.aggregate(Sum('amount')) #adds data
+    
+    #Logic to calculate expenses in last 30 days 
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+    
+     #Logic to calculate expenses in last 7 days
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+    
+    #logic to calculate expenses per day
+    daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+    
+    #logic to calculate expenses by category
+    categorical_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
+    print(categorical_sums)
+    
     expense_form = ExpenseForm()
-    return render(request, 'myapp/index.html', {'expense_form':expense_form, 'expenses': expenses, 'total_expenses': total_expenses})
+    return render(request,'myapp/index.html',{
+        'expense_form':expense_form,
+        'expenses':expenses,
+        'total_expenses':total_expenses,
+        'yearly_sum':yearly_sum,
+        'weekly_sum':weekly_sum,
+        'monthly_sum':monthly_sum,
+        'daily_sums':daily_sums,
+        'categorical_sums':categorical_sums,
+        })
 
 def edit(request, id):
     expense = Expense.objects.get(id=id)
